534c4f29c462fc56632bc7dd5dffb552d560da77
Aubrey Lynn Isaacman <aubrey@lynnfactor.com>
2020-09-24 18:30:21 -0700

    Working on making the volume slider work

---
+++ OFK/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* created by Aubrey Isaacman
+ *
+ * This script controls the settings
+ * Following this Brackeys tutorial: https://www.youtube.com/watch?v=YOaYQrN1oYQ&t=13s&ab_channel=Brackeys
+ */
+
+public class SettingsMenu : MonoBehaviour
+{
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
