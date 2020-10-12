dbe66cf58813e70550fbb99e99cf26b91cacd55c
treverberryman <trever.berryman@gmail.com>
2020-10-12 11:07:21 -0700

    Added Rewired Controller tool, commented out duplicate files to get rid of errors from Twitch code files, deleted duplicat JSON.dll files to remove errors, started on implementing rewired controls for Movie scene

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 /* created by Aubrey Isaacman
  * 
@@ -16,126 +17,159 @@ using UnityEngine;
  *
 */
 
-public class PlayerMovement : MonoBehaviour
-{
 
-    // movement
+public class PlayerMovement : MonoBehaviour {
+
+    //Movement
+    public float speed;
+    public Vector3 movement;
     public KeyCode left;
     public KeyCode right;
+    public float degrees;
 
-    // range of walking speed
-    [Range (0f, 500f)]
-    // set floating speed at 1
-    public float moveSpeed = 1f;
-    Vector2 movement;
-    
-    // rotation
-    public KeyCode rotLeft;
-    public KeyCode rotRight;
-
-    private float rotZ;
-    public int rotSpeed = 0;
-    public Vector3 point; // position of point you want to rotate around
-
-    // player input
-    private float _horizontalInput = 0;
-    private float _verticalInput = 0;
-
-    // this smooch collider
-    public Collider2D smoochZone;
-    public Collider2D otherSmoochZone;
-    // are they smooching now?
-    public bool smoochingNow = false;
-
-    Rigidbody2D rb2D;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    // reference player input
-    public void GetPlayerInput()
-    {
-        //_horizontalInput = Input.GetAxisRaw("Horizontal");
-        //_verticalInput = Input.GetAxisRaw("Vertical");
-    }
-
-    // once per frame
-    void Update()
-    {
-        movement.x = Input.GetAxisRaw("Horizontal");
-
-
-        //GetPlayerInput();
-
-        // move silhouette left
-                // "GetKey" lets you keep doing something even when you're holding the button down
-                // "GetKeyDown" only does it the one time the button is pressed
+    void Update () {
         
-        if(Input.GetKeyDown(left))
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        movement = new Vector3(moveHorizontal, 0f, 0f );
+
+       if(game)
+       
+       if(Input.GetKey(left))
         {
-            transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
-            
+            movement = movement * speed * Time.deltaTime;
+            transform.position += movement;    
         }
         // move silhouette right
-        if(Input.GetKeyDown(right))
+        if(Input.GetKey(right))
         {
-            transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
-            
+            movement = movement * speed * Time.deltaTime;
+            transform.position += movement;
         }
-        
-    }
-
-    // for physics
-    /*
-    void FixedUpdate()
-    {
-        // rotate player by calling that function
-        //RotatePlayer();  
-
-        //rb2D.MovePosition(rb2D.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-    }
-    */
-
-    /*
-    // rotate
-    private void RotatePlayer()
-    {
-        // rotate left
-        if(Input.GetKey(rotLeft))
-        {
-            rotZ += Time.deltaTime * rotSpeed;
-        }
-        // rotate right
-        if(Input.GetKey(rotRight))
-        {
-            rotZ += -Time.deltaTime * rotSpeed;
-        }
-
-        transform.rotation = Quaternion.Euler(0,0, rotZ);
-    }
-    */
-
-    // detect if players are kissing
-    // give feedback for smooching
-    // THIS IS GIVING ME ISSUES, revisit
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-       if(other.gameObject.tag == "Smooch")
-       {
-           // decrease b&w filter by X amount
-
-           
-           // play random kissy sounds
-                // this will probably mean calling a random audio script from KB or Cloud
-           // spawn hearts of random sizes
-
-           // if players have smooched X number of times, spawn random fireworks too
-           Debug.Log("smooching");
-       }
     }
 
 }
+
+// public class PlayerMovement : MonoBehaviour
+// {
+
+//     // movement
+//     public KeyCode left;
+//     public KeyCode right;
+
+//     // range of walking speed
+//     [Range (0f, 500f)]
+//     // set floating speed at 1
+//     public float moveSpeed = 1f;
+//     Vector2 movement;
+    
+//     // rotation
+//     public KeyCode rotLeft;
+//     public KeyCode rotRight;
+
+//     private float rotZ;
+//     public int rotSpeed = 0;
+//     public Vector3 point; // position of point you want to rotate around
+
+//     // player input
+//     private float _horizontalInput = 0;
+//     private float _verticalInput = 0;
+
+//     // this smooch collider
+//     public Collider2D smoochZone;
+//     public Collider2D otherSmoochZone;
+//     // are they smooching now?
+//     public bool smoochingNow = false;
+
+//     Rigidbody2D rb2D;
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb2D = GetComponent<Rigidbody2D>();
+//     }
+
+//     // reference player input
+//     public void GetPlayerInput()
+//     {
+//         _horizontalInput = Input.GetAxisRaw("Horizontal");
+//         _verticalInput = Input.GetAxisRaw("Vertical");
+//     }
+
+//     // once per frame
+//     void Update()
+//     {
+//         movement.x = Input.GetAxisRaw("Horizontal");
+
+
+//         //GetPlayerInput();
+
+//         // move silhouette left
+//                 // "GetKey" lets you keep doing something even when you're holding the button down
+//                 // "GetKeyDown" only does it the one time the button is pressed
+        
+//         if(Input.GetKeyDown(left))
+//         {
+//             transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+            
+//         }
+//         // move silhouette right
+//         if(Input.GetKeyDown(right))
+//         {
+//             transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+            
+//         }
+        
+//     }
+
+//     // for physics
+//     /*
+//     void FixedUpdate()
+//     {
+//         // rotate player by calling that function
+//         //RotatePlayer();  
+
+//         //rb2D.MovePosition(rb2D.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+//     }
+//     */
+
+//     /*
+//     // rotate
+//     private void RotatePlayer()
+//     {
+//         // rotate left
+//         if(Input.GetKey(rotLeft))
+//         {
+//             rotZ += Time.deltaTime * rotSpeed;
+//         }
+//         // rotate right
+//         if(Input.GetKey(rotRight))
+//         {
+//             rotZ += -Time.deltaTime * rotSpeed;
+//         }
+
+//         transform.rotation = Quaternion.Euler(0,0, rotZ);
+//     }
+//     */
+
+//     // detect if players are kissing
+//     // give feedback for smooching
+//     // THIS IS GIVING ME ISSUES, revisit
+//     private void OnTriggerEnter2D(Collider2D other)
+//     {
+//        if(other.gameObject.tag == "Smooch")
+//        {
+//            // decrease b&w filter by X amount
+
+           
+//            // play random kissy sounds
+//                 // this will probably mean calling a random audio script from KB or Cloud
+//            // spawn hearts of random sizes
+
+//            // if players have smooched X number of times, spawn random fireworks too
+//            Debug.Log("smooching");
+//        }
+//     }
+
+
+// }
--- Assets/Twitch/SendEmojiButton.cs
+++ Assets/Twitch/SendEmojiButton.cs
@@ -1,67 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-//using NDream.AirConsole;
-//using Newtonsoft.Json.Linq;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// //using NDream.AirConsole;
+// //using Newtonsoft.Json.Linq;
 
-public class SendEmojiButton : MonoBehaviour
-{
-    [SerializeField] GameObject emojiPrefab;
-    [SerializeField] Canvas canvas;
-    [SerializeField] GameObject audioManager;
-    [SerializeField] float randomRangeMax = 200f;
-    [SerializeField] int emojiNum;
-    private RectTransform canvasRTrans;
-    private AudioManagement amScript;
-    private Vector3 startPos;
+// public class SendEmojiButton : MonoBehaviour
+// {
+//     [SerializeField] GameObject emojiPrefab;
+//     [SerializeField] Canvas canvas;
+//     [SerializeField] GameObject audioManager;
+//     [SerializeField] float randomRangeMax = 200f;
+//     [SerializeField] int emojiNum;
+//     private RectTransform canvasRTrans;
+//     private AudioManagement amScript;
+//     private Vector3 startPos;
     
-/*
-    private void Awake()
-    {
-        AirConsole.instance.onMessage += OnMessage;
-    }
-*/
+// /*
+//     private void Awake()
+//     {
+//         AirConsole.instance.onMessage += OnMessage;
+//     }
+// */
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        canvasRTrans = canvas.GetComponent<RectTransform>();
-        amScript = audioManager.GetComponent<AudioManagement>();
-        startPos = new Vector3(Random.Range(0f, randomRangeMax), 0f, 0f);
-    }
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         canvasRTrans = canvas.GetComponent<RectTransform>();
+//         amScript = audioManager.GetComponent<AudioManagement>();
+//         startPos = new Vector3(Random.Range(0f, randomRangeMax), 0f, 0f);
+//     }
     
-    public void SendEmoji()
-    {
-        Instantiate(emojiPrefab, startPos, Quaternion.identity, canvasRTrans);
-        startPos.x = Random.Range(0f, randomRangeMax);
-    }
+//     public void SendEmoji()
+//     {
+//         Instantiate(emojiPrefab, startPos, Quaternion.identity, canvasRTrans);
+//         startPos.x = Random.Range(0f, randomRangeMax);
+//     }
 
-    public void AddClickCount()
-    {
-        //note: emojiNum starts from 1, not 0!
-        amScript.AddClicks(emojiNum);
-    }
+//     public void AddClickCount()
+//     {
+//         //note: emojiNum starts from 1, not 0!
+//         amScript.AddClicks(emojiNum);
+//     }
 
-/*
-    void OnMessage(int fromDeviceID, JToken data)
-    {
-        Debug.Log("message from" + fromDeviceID + ", data:" + data);
-        if (data["action"] != null && data["action"].ToString().Equals("interact"))
-        {
-            SendEmoji();
-        }
-    }
-*/
+// /*
+//     void OnMessage(int fromDeviceID, JToken data)
+//     {
+//         Debug.Log("message from" + fromDeviceID + ", data:" + data);
+//         if (data["action"] != null && data["action"].ToString().Equals("interact"))
+//         {
+//             SendEmoji();
+//         }
+//     }
+// */
 
-/*
-    private void OnDestroy()
-    {
-        //unregistered events
-        if (AirConsole.instance != null)
-        {
-            AirConsole.instance.onMessage -= OnMessage;
-        }
-    }
-*/
-}
+// /*
+//     private void OnDestroy()
+//     {
+//         //unregistered events
+//         if (AirConsole.instance != null)
+//         {
+//             AirConsole.instance.onMessage -= OnMessage;
+//         }
+//     }
+// */
+// }
