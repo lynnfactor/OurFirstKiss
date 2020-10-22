39341cad6f75d0c101ca2621dedd6de9ed99de4d
treverberryman <trever.berryman@gmail.com>
2020-10-22 11:14:00 -0700

    Somewhat working second pass for movement. Might not be scaleable

--- Assets/Asset Store things/Uduino/Examples/Advanced/SendBundle/Bundle.cs
+++ Assets/Asset Store things/Uduino/Examples/Advanced/SendBundle/Bundle.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Uduino;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using Uduino;
 
-public class Bundle : MonoBehaviour {
+// public class Bundle : MonoBehaviour {
 
-    UduinoManager u;
+//     UduinoManager u;
 
-	void Start ()
-    {
-        u = UduinoManager.Instance;
+// 	void Start ()
+//     {
+//         u = UduinoManager.Instance;
 
-        for(int i=2;i < 13;i++)
-        {
-            u.pinMode(i, PinMode.Output);
-        }
+//         for(int i=2;i < 13;i++)
+//         {
+//             u.pinMode(i, PinMode.Output);
+//         }
 
-        StartCoroutine(BlinkAllLoop());
-    }
+//         StartCoroutine(BlinkAllLoop());
+//     }
 
-    IEnumerator BlinkAllLoop()
-    {
-        while (true)
-        {
-            for (int i = 2; i < 11; i++)
-            {
-                u.digitalWrite(i, State.HIGH,"LedOn");
-            }
-           u.SendBundle("LedOn");
-            yield return new WaitForSeconds(1);
-            for (int i = 2; i < 11; i++)
-            {
-                u.digitalWrite(i, State.LOW, "LedOff");
-            }
-           u.SendBundle("LedOff");
-            yield return new WaitForSeconds(1);
-        }
-    }
-}
+//     IEnumerator BlinkAllLoop()
+//     {
+//         while (true)
+//         {
+//             for (int i = 2; i < 11; i++)
+//             {
+//                 u.digitalWrite(i, State.HIGH,"LedOn");
+//             }
+//            u.SendBundle("LedOn");
+//             yield return new WaitForSeconds(1);
+//             for (int i = 2; i < 11; i++)
+//             {
+//                 u.digitalWrite(i, State.LOW, "LedOff");
+//             }
+//            u.SendBundle("LedOff");
+//             yield return new WaitForSeconds(1);
+//         }
+//     }
+// }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,222 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
 
-/* created by Aubrey Isaacman
- * 
- * Following this tutorial specifically for rotation: https://www.youtube.com/watch?v=lJ1jv5OhZZE
- * Following "Complete C# Unity Game Developer 2D" course for player movement
- *
- * This script moves the players right and left
- * and rotates them to the left or to the right (to lean in for smooches)
-        * As of 2020.09.03, Aubrey commented out rotation
- * 
- * TEMPORARILY works only with KEYBOARD PRESS
- * Eventually this script will be controlled via accelerometer (rotation) and proximity sensor (movement)
- *
-*/
-
-
 public class PlayerMovement : MonoBehaviour {
 
-
-    //Movement
-    private float moveH;
-    private Vector3 movement;
-    public float degrees, speed;
-
-    [Header("Rewired")]
+	[Header("Rewired")]
     public Player rewiredPlayer;
     public int playerID;
-    public bool isPlayer1 = false;
-    private string playerName;
 
-    //initializes controls for Rewired
-    private void InitializeControls()
+	[Header("Movement")]
+	private float moveH;	
+	public float amountToMoveModifier;
+	Vector3 amountToMove;
+	public float minimum = 5.0f;
+	public float maximum = 10.0f;
+	public float duration = 5.0f;
+	float startTime;
+	 public bool collided;
+
+	[Header("Rotation")]
+	public float speed = 2f;
+	public float time;
+	public float maxRotation = 45f;
+
+	void Start() {
+		startTime = Time.time;
+		Debug.Log("Player " + playerID + " Position: " + transform.position.x);
+	}
+
+	private void InitializeControls()
     {
-        
         rewiredPlayer = ReInput.players.GetPlayer(playerID);
-
     }
 
-    void GetMovementInput()
+	private void Awake()
     {
-
-        moveH = rewiredPlayer.GetAxisRaw("Horizontal");
-
-    }
-
-    private void Awake()
-    {
-        //initializes controls
+        //Initializes controls
         InitializeControls();
-
-        if (isPlayer1)
-        {
-
-            playerName = "Player1";
-
-        }
-        else
-        {
-
-            playerName = "Player2";
-
-        }
     }
 
-    void Update () {
-
-        GetMovementInput();
-        movement = new Vector2(moveH,0f);
-        
-        if(rewiredPlayer.GetNegativeButton("Horizontal")) {
-            movement = movement * moveH * speed * Time.deltaTime;
-            transform.position -= movement;
-        }
-
-        if(rewiredPlayer.GetButton("Horizontal")) {
-            movement = movement * moveH * speed * Time.deltaTime;
-            transform.position += movement;
-        }
-            
-
-    //    
-       
-    //    if(Input.GetKey(left))
-    //     {
-    //             
-    //     }
-    //     // move silhouette right
-    //     if(Input.GetKey(right))
-    //     {
-    //         movement = movement * moveH * Time.deltaTime;
-    //         transform.position += movement;
-    //     }
+	void GetMovementInput()
+    {
+        moveH = rewiredPlayer.GetAxisRaw("Horizontal");
     }
 
+	void Update () {
+		Move ();
+	}
+
+	// void OnTriggerEnter(Collider2D other)
+	// {
+	// 	if (other.gameObject.tag == "Player")
+    //     	collided = true;
+	// }
+
+	void Move()
+	{
+		amountToMove = new Vector3(amountToMoveModifier,0,0);
+
+
+		if(transform.position.x < -8.8 && rewiredPlayer.GetNegativeButtonDown("Horizontal")) {
+			Debug.Log("Player " + playerID + " Position: " + transform.position.x);
+		}
+		else if(transform.position.x > 8.8 && rewiredPlayer.GetButtonDown("Horizontal")) {
+			Debug.Log("Player " + playerID + " Position: " + transform.position.x);
+		}
+		else if(transform.position.x >= -8.8 || transform.position.x <= 8.8) {
+			if(rewiredPlayer.GetButtonDown("Horizontal")) {
+				transform.position = Vector3.Lerp(transform.position, transform.position + amountToMove, 1);
+				// transform.rotation = Quaternion.Euler(maxRotation * Mathf.Sin(Time.time * speed * time), 0f, 0f);
+				Debug.Log("Player " + playerID + " Position: " + transform.position.x);
+			}
+			else if(rewiredPlayer.GetNegativeButtonDown("Horizontal")) {
+				transform.position = Vector3.Lerp(transform.position, transform.position + amountToMove * -1.0f, 1);
+				// transform.rotation = Quaternion.Euler(maxRotation * Mathf.Sin(Time.time * speed * time), 0f, 0f);
+				Debug.Log("Player " + playerID + " Position: " + transform.position.x);
+			}
+		}
+	}
 }
-
-// public class PlayerMovement : MonoBehaviour
-// {
-
-//     // movement
-//     public KeyCode left;
-//     public KeyCode right;
-
-//     // range of walking speed
-//     [Range (0f, 500f)]
-//     // set floating speed at 1
-//     public float moveSpeed = 1f;
-//     Vector2 movement;
-    
-//     // rotation
-//     public KeyCode rotLeft;
-//     public KeyCode rotRight;
-
-//     private float rotZ;
-//     public int rotSpeed = 0;
-//     public Vector3 point; // position of point you want to rotate around
-
-//     // player input
-//     private float _horizontalInput = 0;
-//     private float _verticalInput = 0;
-
-//     // this smooch collider
-//     public Collider2D smoochZone;
-//     public Collider2D otherSmoochZone;
-//     // are they smooching now?
-//     public bool smoochingNow = false;
-
-//     Rigidbody2D rb2D;
-
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         rb2D = GetComponent<Rigidbody2D>();
-//     }
-
-//     // reference player input
-//     public void GetPlayerInput()
-//     {
-//         _horizontalInput = Input.GetAxisRaw("Horizontal");
-//         _verticalInput = Input.GetAxisRaw("Vertical");
-//     }
-
-//     // once per frame
-//     void Update()
-//     {
-//         movement.x = Input.GetAxisRaw("Horizontal");
-
-
-//         //GetPlayerInput();
-
-//         // move silhouette left
-//                 // "GetKey" lets you keep doing something even when you're holding the button down
-//                 // "GetKeyDown" only does it the one time the button is pressed
-        
-//         if(Input.GetKeyDown(left))
-//         {
-//             transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
-            
-//         }
-//         // move silhouette right
-//         if(Input.GetKeyDown(right))
-//         {
-//             transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
-            
-//         }
-        
-//     }
-
-//     // for physics
-//     /*
-//     void FixedUpdate()
-//     {
-//         // rotate player by calling that function
-//         //RotatePlayer();  
-
-//         //rb2D.MovePosition(rb2D.position + movement * moveSpeed * Time.fixedDeltaTime);
-
-//     }
-//     */
-
-//     /*
-//     // rotate
-//     private void RotatePlayer()
-//     {
-//         // rotate left
-//         if(Input.GetKey(rotLeft))
-//         {
-//             rotZ += Time.deltaTime * rotSpeed;
-//         }
-//         // rotate right
-//         if(Input.GetKey(rotRight))
-//         {
-//             rotZ += -Time.deltaTime * rotSpeed;
-//         }
-
-//         transform.rotation = Quaternion.Euler(0,0, rotZ);
-//     }
-//     */
-
-//     // detect if players are kissing
-//     // give feedback for smooching
-//     // THIS IS GIVING ME ISSUES, revisit
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//        if(other.gameObject.tag == "Smooch")
-//        {
-//            // decrease b&w filter by X amount
-
-           
-//            // play random kissy sounds
-//                 // this will probably mean calling a random audio script from KB or Cloud
-//            // spawn hearts of random sizes
-
-//            // if players have smooched X number of times, spawn random fireworks too
-//            Debug.Log("smooching");
-//        }
-//     }
-
-
-// }
