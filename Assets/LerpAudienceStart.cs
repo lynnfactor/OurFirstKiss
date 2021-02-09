9a6776b1102e83e33eb7895dc871bb5636be153a
lyu20 <32178113+lyu20@users.noreply.github.com>
2021-02-09 13:39:14 -0500

    animate person walking in

---
+++ Assets/LerpAudienceStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpAudienceStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //[SerializeField]
+    private Animation anim;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+ 
+}
