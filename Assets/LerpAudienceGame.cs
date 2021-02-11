f26ebb06ff0b5792e0302e729061945ffc4a07c7
lyu20 <32178113+lyu20@users.noreply.github.com>
2021-02-11 12:19:28 -0500

    finished setting up audience member sitting between players
    if the players have dist 12 or more an audience member sits in between them.
    something weird started happening after i pulled origin, where the players moved on their own....

--- Assets/LerpAudienceGame.cs
+++ Assets/LerpAudienceGame.cs
@@ -5,15 +5,40 @@ using UnityEngine;
 public class LerpAudienceGame : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Animation animEnter;
+    private Animator animator;
+    public GameObject P1;
+    public GameObject P2;
+    private int frames;
+    bool enter = false;
     void Start()
     {
-        
+        frames = 0;
+        animator = GetComponent<Animator>();
+        animator.SetBool("enter", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        while(Mathf.Abs(P1.transform.position.x - P2.transform.position.x) > 12f && !enter){
+            frames++;
+            if(frames > 60){
+                frames = 0;
+                enterAudience();
+            }
+        }
+    }
+
+    public void enterAudience(){
+        enter = true;
+
+        animator.SetBool("enter", true);
+        StartCoroutine(WaitAudience());
+    }
+
+    IEnumerator WaitAudience(){
+        yield return new WaitForSeconds(4f);
+        animator.SetBool("isLeaving", true);
+        animator.SetBool("enter", false);
     }
 }
