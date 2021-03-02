54c630e5780f6f096c37a253b7579122c3755fee
lyu20 <32178113+lyu20@users.noreply.github.com>
2021-03-02 14:58:16 -0500

    working on accessory select
    weird issue where the accessory name gets changed between scenes?

--- Assets/Scripts/swapAccessory.cs
+++ Assets/Scripts/swapAccessory.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class swapAccessory : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -9,19 +9,31 @@ public class swapAccessory : MonoBehaviour
     private SpriteRenderer rend; // this object's sprite renderer
     public GameObject P1dec;
     public GameObject P2dec;
+    //private Sprite[] all;
     void Start()
     {
         rend = gameObject.GetComponent<SpriteRenderer>();
+        PlayerPrefs.SetString("P1 accessory", "decorations-04_1");
+        PlayerPrefs.SetString("P2 accessory", "decorations-04_2");
+        //all = Resources.LoadAll<Sprite>("accessories");
     }
 
-    // Update is called once per frame
+    // swap the accessory when the player moves seats
     void OnTriggerEnter2D(Collider2D other){
+        //Debug.Log(gameObject.name);
         if (other.name == "P1"){
             P1dec.GetComponent<SpriteRenderer>().sprite = rend.sprite;
+            Debug.Log("P1: " + rend.sprite.name);
+            PlayerPrefs.SetString("P1 accessory", rend.sprite.name);
+            
         }
-        else if (other.name == "P2"){
+        if (other.name == "P2"){
             P2dec.GetComponent<SpriteRenderer>().sprite = rend.sprite;
+            Debug.Log("P2: " + rend.sprite.name);
+            PlayerPrefs.SetString("P2 accessory", rend.sprite.name);
+            
         }
         main.SpawnNextAccessory(rend);
+        
     }
 }
