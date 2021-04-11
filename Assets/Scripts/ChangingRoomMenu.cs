c7669ca024146d2bbec8bbd28dee8187b77b9367
lyu20 <32178113+ls-yu@users.noreply.github.com>
2021-04-11 18:32:54 -0400

    fixed changing room menu
    added functionality to Play, Options, and Quit buttons.
    ChangingRoomMenu.cs

---
+++ Assets/Scripts/ChangingRoomMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+* By Lex Yu, lsyu@usc.edu
+*/
+public class ChangingRoomMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("scene2-Movie");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
